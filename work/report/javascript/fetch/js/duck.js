85480fb6da2bae1c238cbc4d7d0e9b59a441d94c
Nilay Yilmaz <nilay.yilmaz0002@stud.hkr.se>
2020-06-06 00:51:20 +0200

    duck

--- work/report/javascript/fetch/js/duck.js
+++ work/report/javascript/fetch/js/duck.js
@@ -5,12 +5,13 @@ var element = document.getElementById("duck");
 
 element.addEventListener("click" , function()
 {
-    element.innerHTML = element.innerHTML + "<p>HELLO</p>";
+   
     console.log("Duck clicked!");
 });
 element.addEventListener("mouseover" , function()
 {
-   if(element.offsetLeft < 980) element.style.left = element.offsetLeft + 20 + "px";
+   if(element.offsetLeft < 1000 )element.style.left = element.offsetLeft + 20 + "px";
+   if(element.offsetLeft == 495 )element.style.top = element.offsetTop + 20 + "px";
     console.log("Mouse Over!");
     console.log(element.style.left);
     console.log(element.offsetLeft);
