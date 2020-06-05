0ca7fb456616e3e037a16dbfcbd0dedca5f2bd3c
Nilay Yilmaz <nilay.yilmaz0002@stud.hkr.se>
2020-06-06 00:13:28 +0200

    duck

--- work/report/javascript/fetch/js/duck.js
+++ work/report/javascript/fetch/js/duck.js
@@ -2,6 +2,19 @@
 
 (function(){
 var element = document.getElementById("duck");
+
+element.addEventListener("click" , function()
+{
+    element.innerHTML = element.innerHTML + "<p>HELLO</p>";
+    console.log("Duck clicked!");
+});
+element.addEventListener("mouseover" , function()
+{
+    element.style.left = element.offsetLeft + 20 + "px";
+    console.log("Mouse Over!");
+    console.log(element.style.left);
+    console.log(element.offsetLeft);
+});
 console.log(element);
 console.log("Duck ready!!");
 
