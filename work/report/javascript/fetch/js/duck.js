1e8bff37ee5c770bb683307a1d26c3f8d3382334
Nilay Yilmaz <nilay.yilmaz0002@stud.hkr.se>
2020-06-09 00:55:02 +0200

    kf

--- work/report/javascript/fetch/js/duck.js
+++ work/report/javascript/fetch/js/duck.js
@@ -17,9 +17,9 @@ console.log("Duck ready!!");
 element.addEventListener("dblclick" , function()
 {
 console.log("Double Clicked!");
-if (element.style.visibility = 'visible') {
+if (element.style.visibility === 'visible') {
   element.style.visibility = 'hidden';
-} else {
+} else if(element.style.visibility === 'hidden') {
   element.style.visibility = 'visible';
 }
 });
